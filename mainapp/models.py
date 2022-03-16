6f52668c5d8acf0c38ad56aa43653f3f96ca10d7
Ayseke88 <91011188+Ayseke88@users.noreply.github.com>
2022-03-16 21:39:16 +0600

    добавила description в базу данных, models, сделала migration

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,10 @@ class Items(models.Model):
     category = models.CharField(max_length=30, blank=True, null=True)
     price = models.CharField(max_length=30, blank=True, null=True)
     image = models.CharField(max_length=150, blank=True, null=True)
+    description = models.TextField(blank=True)
+
+
+
 
     def __str__(self):
         return self.name + " " + self.category
