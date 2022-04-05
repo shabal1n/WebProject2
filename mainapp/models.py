c3a9a398bff2a9a5ea82884d4f948581e8743d5a
shabal1n <artur.shab@yandex.ru>
2022-04-05 14:33:44 +0600

    Fixed everything

--- mainapp/models.py
+++ mainapp/models.py
@@ -47,7 +47,7 @@ class Items(models.Model):
     brand = models.ForeignKey(Brands, blank=True, null=True, on_delete=models.CASCADE, verbose_name='BRAND')
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE, verbose_name='CATEGORY')
     price = models.IntegerField(blank=True, null=True, verbose_name='PRICE')
-    image = models.ImageField(null=True, blank=True,  )
+    image = models.ImageField(null=True, blank=True, )
     size = models.ManyToManyField(Sizes)
     stock = models.IntegerField(verbose_name='STOCK')
     description = models.TextField(blank=True)
@@ -118,23 +118,25 @@ class DeliveryCompany(models.Model):
         verbose_name_plural = 'Delivery Companies'
 
 
-#Product Review
-RATING=(
-    (1, '1'),
-    (2,'2'),
-    (3,'3'),
-    (4,'4'),
-    (5,'5'),
+# Product Review
+RATING = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5)
 )
+
+
 class ProductReview(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
-    product=models.ForeignKey(Items, on_delete=models.CASCADE)
-    review_text=models.TextField()
-    review_rating=models.CharField(choices=RATING, max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Items, on_delete=models.CASCADE)
+    review_text = models.TextField()
+    review_rating = models.IntegerField(choices=RATING, max_length=150)
 
     class Meta:
         verbose_name = 'Review'
         verbose_name_plural = 'Reviews'
 
     def get_review_rating(self):
-     return self.review_rating
+        return self.review_rating
