b7f625c9e15d6fedbb1024e59168928a468d625b
shabal1n <artur.shab@yandex.ru>
2022-05-11 23:37:56 +0600

    Added stock to item

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import JsonResponse, HttpResponseRedirect
 from django.contrib import messages
-from .models import Items, ProductReview, Basket, BasketItem, Order, DeliveryCompany, Brands
+from .models import Items, ProductReview, Basket, BasketItem, Order, DeliveryCompany
 from .forms import ReviewAdd, ContactForm
 from django.db.models import Q
 from django.core.mail import send_mail, get_connection
