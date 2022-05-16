a4103146c07513616c565359e98964919aa42b40
shabal1n <artur.shab@yandex.ru>
2022-05-16 22:56:29 +0800

    Map was added

--- static/script/menu.js
+++ static/script/menu.js
@@ -1,7 +1,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('links-container')[0]
-const navL = document.querySelectorAll('links-container li') ;
+const navL = document.querySelectorAll('links-container li');
 
-toggleButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-  })
+// toggleButton.addEventListener('click', () => {
+//     navLinks.classList.toggle('active')
+//   })
